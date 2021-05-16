b3afe93f5a475b28af74869669bd845aa53b371f
kylokid2k7 <81998409+kylokid2k7@users.noreply.github.com>
2021-05-16 10:01:45 -0400

    Create jar.v.js
    this is my Jarvis ai  bot   that i made i really hope you guys enjoy it  make sure to subscribe to my Chanel k bye for now

---
+++ jar.v.js
@@ -0,0 +1,17 @@
+j.A.R.V.I.S.EXE                                                               
+J.A.R.V.I.S VERSION 1.16.5 RELEASE VERSION APPLY                                           
+J.A.R.V.I.S_ ai asitant IOS_PC SECURITY SYSTEM apply
+J.A.R.V.I.S url-httpslocalhost:4000 apply                    
+J.A.R.V.I.S [ text apply hello again sir  welcome back 
+J.A.R.V.I.S ai asistant  command apply sir sir                    
+J.A.R.V.I.S command APPLY hey J.A.R.V.I.S  open new text document
+J.A.R.V.I.S command APPLY  hey J.A.R.V.I.S open minecraft launcher 
+J.A.R.V.I.S  command APPLY  hey J.A.R.V.I.S  open settings            
+J.a.r.v.i.s COMMAND APPLY  HEY J.a.r.v.i.S  ACTIVATE LOCK  DOWN SEQUENCE 
+j.a.r.V.i.s COMMAND APPLY  HEY j.A.R.V.i.s  TURN ALL OFF LIGHTS 
+[ save as j.a.r.v.i.s .exe script .text
+    
+
+] 
+ ]  	      ]   
+                 ]
